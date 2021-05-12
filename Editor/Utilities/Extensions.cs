f6df5f3b126fdea1bf148a1cf03286757bdb74c0
Fofanius <vova19007@gmail.com>
2021-05-12 20:49:57 +0300

    Остальные сцены, runtime использование
    - добавил вывод сцен, которые на распиханы по библиотекам
    - добавил возможность использовать библиотеку в runtime
    - начальные методы расширения для использования библиотек в runtime

--- Editor/Utilities/Extensions.cs
+++ Editor/Utilities/Extensions.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SceneHub.Utilities
 {
     internal static class Extensions
     {
-        internal static bool IsNullOrEmpty<T>(this ICollection<T> collection) => collection == null || collection.Count == default;
+        internal static bool IsNullOrEmpty<T>(this IEnumerable<T> collection) => collection == null || collection.Count() == default;
+
+        internal static string GetLibraryDisplayName(this SceneLibrary library)
+        {
+            if (library)
+            {
+                return string.IsNullOrWhiteSpace(library.Title) ? $"[{library.name}]" : $"{library.Title} [{library.name}]";
+            }
+
+            throw new ArgumentNullException(nameof(library));
+        }
+
+        internal static string GetSceneInfoDisplayName(this SceneInfo sceneInfo)
+        {
+            if (sceneInfo == null) throw new ArgumentNullException(nameof(sceneInfo));
+            return string.IsNullOrWhiteSpace(sceneInfo.Title) ? $"[{sceneInfo.SceneName}]" : $"{sceneInfo.Title} [{sceneInfo.SceneName}]";
+        }
     }
 }
--- Editor/Utilities/SceneManagementUtility.cs
+++ Editor/Utilities/SceneManagementUtility.cs
@@ -3,18 +3,18 @@ using UnityEditor.SceneManagement;
 
 namespace SceneHub.Utilities
 {
-    public static class SceneManagmentUtility 
+    internal static class SceneManagementUtility
     {
         /// <summary>
         /// Сохраняет текущую активную сцену и осуществляет переход на заданную.
         /// </summary>
         public static void ChangeScene(SceneAsset scene)
         {
-            if(!scene) return;
-            
+            if (!scene) return;
+
             // сохраняем текущую активну
             EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
-            
+
             // переходим на новую
             var path = AssetDatabase.GetAssetPath(scene);
             EditorSceneManager.OpenScene(path);
